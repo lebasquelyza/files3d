0349b5461837ddb001b2d45efc7472a1791d13ab
lebasquelyza <lebasquelyza66@gmail.com>
2025-10-30 17:48:00 +0100

    Update next.config.mjs

--- ai-fitness-demo-netlify-static/next.config.mjs
+++ ai-fitness-demo-netlify-static/next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  experimental: {
-    optimizePackageImports: ['@tensorflow/tfjs', '@tensorflow-models/pose-detection']
-  }
+  reactStrictMode: true,
 };
+
 export default nextConfig;
